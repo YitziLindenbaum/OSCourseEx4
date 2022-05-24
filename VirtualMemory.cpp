7d073cbd6ee265431cdd33df449b6d00af11b763
chucklind <yitzchak.lindenbaum@mail.huji.ac.il>
2022-05-24 20:00:26 +0300

    Added comments etc

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -5,7 +5,7 @@
 #define RIGHTMOST_BITS(num) (num & (PAGE_SIZE - 1))
 
 
-word_t findEmptyFrameBelow(word_t frame, int depth, word_t exclude, word_t *max_frame_index);
+word_t findEmptyFrameBelow(word_t frame, uint64_t parent, word_t exclude, word_t *max_frame_index, int depth);
 word_t findEmptyFrame(word_t exclude, word_t *max_frame_index);
 
 void VMinitialize() {
@@ -28,15 +28,17 @@ int VMread(uint64_t virtualAddress, word_t* value) {
         if (child_frame == 0) {
             child_frame = findEmptyFrame(parent_frame, &max_frame_index);
         }
+
         // no empty frames available, try using unused frame
         if (child_frame == 0 && max_frame_index + 1 < NUM_FRAMES) {
             child_frame = max_frame_index + 1;
         }
 
+        // all frames are used and none are empty, we must evict a page.
         if (child_frame == 0) {
             // TODO continue here
         }
-
+        PMwrite((parent_frame * PAGE_SIZE) + RIGHTMOST_BITS(virtualAddress >> curBits), child_frame);
         parent_frame = child_frame;
         // @todo in theory, two addresses can be unified into the same variable?
     }
@@ -45,7 +47,16 @@ int VMread(uint64_t virtualAddress, word_t* value) {
     return 0;
 }
 
-word_t findEmptyFrameBelow(word_t frame, int depth, word_t exclude, word_t *max_frame_index) {
+/**
+ * Recursive helper function for findEmptyFrame -- finds an empty frame below the given one in the VM tree,
+ * if one exists.
+ * @param frame The frame from which to start DFS
+ * @param depth Level of the tree the starting frame is in
+ * @param exclude Frame to exclude (not to be considered empty)
+ * @param max_frame_index Output argument -- maximal index of a used frame.
+ * @return Index of an empty frame, or 0 if none exist.
+ */
+word_t findEmptyFrameBelow(word_t frame, uint64_t parent, word_t exclude, word_t *max_frame_index, unsigned int depth) {
 
     if (depth == TABLES_DEPTH || frame == exclude) {
         return 0;
@@ -58,7 +69,7 @@ word_t findEmptyFrameBelow(word_t frame, int depth, word_t exclude, word_t *max_
         PMread((frame * PAGE_SIZE) + i, &holder2);
         if (holder2 > 0) {
             if (holder2 > *max_frame_index) {*max_frame_index = holder2;}
-            word_t emptyFrame = findEmptyFrameBelow(holder2, depth + 1, exclude, max_frame_index);
+            word_t emptyFrame = findEmptyFrameBelow(holder2, (frame * PAGE_SIZE) + i, exclude, max_frame_index, depth + 1);
             if (emptyFrame > 0) {
                 return emptyFrame;
             }
@@ -68,11 +79,13 @@ word_t findEmptyFrameBelow(word_t frame, int depth, word_t exclude, word_t *max_
     if (holder1 > 0) {
         return 0;
     }
+    // the frame given as an argument points to all zeros -- remove it from its parent and return it.
+    PMwrite(parent, 0);
     return frame;
 }
 
 word_t findEmptyFrame(word_t exclude, word_t *max_frame_index) {
-    return findEmptyFrameBelow(0, 0, exclude, max_frame_index);
+    return findEmptyFrameBelow(0, 0, exclude, max_frame_index, 0);
 }
 
 
